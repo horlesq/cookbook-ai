f2af737c3898725d01139431d4028884a955f468
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2025-10-17 20:53:02 +0300

    error message for debugging production

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -37,9 +37,6 @@ export async function POST(request) {
         );
     } catch (error) {
         console.error("Registration error:", error);
-        return NextResponse.json(
-            { error: "Internal server error" },
-            { status: 500 }
-        );
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
